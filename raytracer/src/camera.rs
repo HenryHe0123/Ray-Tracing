e17268ff65f0a066435885abdfb512d50f7062ee
HenryHe0123 <13600668331@163.com>
2023-07-07 19:41:44 +0800

    generate book1/image18

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -1,5 +1,5 @@
 use crate::ray::Ray;
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::{cross, Point3, Vec3};
 use std::f64;
 
 #[derive(Debug, Copy, Clone)]
@@ -11,18 +11,26 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
-        // vfov: vertical field-of-view in degrees
+    pub fn new(
+        lookfrom: &Point3,
+        lookat: &Point3,
+        vup: &Vec3,
+        vfov: f64, //vertical field-of-view in degrees
+        aspect_ratio: f64,
+    ) -> Self {
         let theta = vfov.to_radians() / 2.0;
         let h = theta.tan();
         let viewport_h = 2.0 * h;
         let viewport_w = aspect_ratio * viewport_h;
-        let focal_length = 1.0;
-        let origin = Point3::default();
-        let horizontal = Vec3::new(viewport_w, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_h, 0.0);
-        let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        let w = (*lookfrom - *lookat).unit();
+        let u = cross(vup, &w).unit();
+        let v = cross(&w, &u).unit();
+
+        let origin = *lookfrom;
+        let horizontal = viewport_w * u;
+        let vertical = viewport_h * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
 
         Camera {
             origin,
@@ -35,11 +43,5 @@ impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         let dir = self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin;
         Ray::new(&self.origin, &dir)
-    }
-}
-
-impl Default for Camera {
-    fn default() -> Self {
-        Self::new(90.0, 16.0 / 9.0)
     }
 }
