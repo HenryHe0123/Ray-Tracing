0b249b53c8032200664390b857f1d071f4ce270d
HenryHe0123 <13600668331@163.com>
2023-07-10 16:57:06 +0800

    generate book2/image9

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -26,9 +26,12 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let u = p.x() - p.x().floor();
-        let v = p.y() - p.y().floor();
-        let w = p.z() - p.z().floor();
+        let mut u = p.x() - p.x().floor();
+        let mut v = p.y() - p.y().floor();
+        let mut w = p.z() - p.z().floor();
+        u = u * u * (3.0 - 2.0 * u);
+        v = v * v * (3.0 - 2.0 * v);
+        w = w * w * (3.0 - 2.0 * w);
 
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
