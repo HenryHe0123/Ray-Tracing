887cfb431cc5ebd1611372516c47d32101fdc2aa
HenryHe0123 <13600668331@163.com>
2023-07-10 16:54:31 +0800

    generate book2/image8

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -26,10 +26,27 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = ((4.0 * p.x()) as i32 & 255) as usize;
-        let j = ((4.0 * p.y()) as i32 & 255) as usize;
-        let k = ((4.0 * p.z()) as i32 & 255) as usize;
-        self.ranfloat[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
+
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+
+        for (di, ci) in c.iter_mut().enumerate() {
+            for (dj, cj) in ci.iter_mut().enumerate() {
+                for (dk, ck) in cj.iter_mut().enumerate() {
+                    *ck = self.ranfloat[(self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize])
+                        as usize];
+                }
+            }
+        }
+
+        Perlin::trilinear_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm() -> Vec<i32> {
@@ -46,6 +63,21 @@ impl Perlin {
             let target = random_int_range(0, i as i32);
             p.swap(i, target as usize);
         }
+    }
+
+    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for (i, ci) in c.iter().enumerate() {
+            for (j, cj) in ci.iter().enumerate() {
+                for (k, ck) in cj.iter().enumerate() {
+                    accum += ((i as f64) * u + ((1 - i) as f64) * (1.0 - u))
+                        * ((j as f64) * v + ((1 - j) as f64) * (1.0 - v))
+                        * ((k as f64) * w + ((1 - k) as f64) * (1.0 - w))
+                        * (*ck);
+                }
+            }
+        }
+        accum
     }
 }
 
